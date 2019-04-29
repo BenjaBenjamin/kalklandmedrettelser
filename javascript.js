3c206291c99cdb3f7390c52cf09151e1a87cae73
benjabenjamin <benj4117@kea.stud.dk>
2019-04-29 13:43:14 +0200

    Kalkland

---
+++ javascript.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", billede);
+
+function billede(){
+    console.log(billede);
+
+    document.querySelector("h1").addEventListener("click", link)
+}
+
+function link(){
+    console.log(link);
+
+
+}
